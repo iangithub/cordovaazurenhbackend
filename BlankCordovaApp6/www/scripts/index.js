62f09e6a5e38b4e2466a048c0431f16907d56cd3
iangithub <kmcchen@seed.net.tw>
2016-07-13 15:38:40 +0800

    create

---
+++ BlankCordovaApp6/www/scripts/index.js
@@ -0,0 +1,85 @@
+﻿// 如需空白範本的簡介，請參閱下列文件: 
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// 若要針對在 Ripple 或 Android 裝置/模擬器上載入的頁面，偵錯程式碼: 請啟動您的應用程式，設定中斷點，
+// 然後在 JavaScript 主控台中執行 "window.location.reload()"。
+(function () {
+    "use strict";
+
+    document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+
+    function onDeviceReady() {
+        // 處理 Cordova 暫停與繼續事件
+        document.addEventListener('pause', onPause.bind(this), false);
+        document.addEventListener('resume', onResume.bind(this), false);
+
+        var azureClient = new WindowsAzure.MobileServiceClient('http://ianmobileapp2.azurewebsites.net');
+
+        //PushNotification.
+        var push = PushNotification.init({
+            android: {
+                senderID: "your google project code"
+            },
+            ios: {
+                alert: "true",
+                badge: "true",
+                sound: "true",
+            },
+            windows: {}
+        });
+
+        /*
+        原本使用WindowsAzure.MobileServiceClient來實作註冊Azure NH
+        但MobileService.Cordova.js / executeRequest方法中的'push/installations/'
+        uri 似乎是錯誤，目前皆會回傳404導致無法進行Azure NH註冊
+        */
+        //push.on('registration', function (data) {
+        //    console.log('PNS data'+data.registrationId);
+        //    azureClient.push.register('gcm', data.registrationId);
+        //});
+
+
+        push.on('registration', function (data) {
+            //取得pns值，各平台的pns值不同
+            var pnsid = data.registrationId;
+
+            /*
+                自建mobile app backend，開放API來做註冊Azure HN的動作
+                取代MobileService plugin 回應 404 的問題
+            */
+            $.post("http://your-mobile-abb-backend.azurewebsites.net/api/register/post",
+                function (data, status) {
+                    //取得 Azure HN 註冊ID值
+                    console.log(data + ";" + status);
+
+                    $.ajax({
+                        type: 'put',
+                        url: 'http://ianmobileapp2.azurewebsites.net/api/register/Put/?id=' + data,
+                        data: {
+                            platform: 'gcm',
+                            Handle: pnsid,
+                            Tags: ''
+                        }
+                    })
+                });
+        });
+
+        push.on('notification', function (data, d2) {
+            alert('Push Received: ' + data.message);
+        });
+
+
+        push.on('error', function (e) {
+            console.log('push.on Error:' + e.message);
+        })
+
+    };
+
+    function onPause() {
+        // TODO: 這個應用程式已暫停。請在這裡儲存應用程式狀態。
+    };
+
+    function onResume() {
+        // TODO: 這個應用程式已重新啟動。請在這裡還原應用程式狀態。
+    };
+
+})();
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # cordovaazurenhbackend
 無法使用mobile service plugin結合azure PushNotification 的替代方案
+
+
+Cordova App 要使用 Azure Notification Hubs服務，正常來說可以用透過 "cordova-plugin-ms-azure-mobile-apps"  + "phonegap-plugin-push" 這二個plugin來達成，詳細可以參考我放在github的範例(https://github.com/iangithub/Cordova-Azure-Notification-Hubs)，但這幾天一位朋友告知好像無法正常運作，會有404錯誤，本篇提供另一個替代方案，讓Cordova App可以正常使用Azure Notification Hubs服務
